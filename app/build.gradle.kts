2d5d5e11467d99926985418b298dbcb647ef9c58
Smart <startechh030@gmail.com>
2026-06-29 15:04:02 +0100

    Auth integration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -142,6 +142,7 @@ dependencies {
     implementation(libs.supabase.auth)
     implementation(libs.supabase.postgrest)
     implementation(libs.supabase.compose.auth)
+    implementation(libs.supabase.functions)
     implementation(libs.ktor.client.android)
     implementation(libs.ktor.client.core)
     implementation(libs.ktor.utils)
